e5778501f139e4aaff9b827e5a890e62841c524b
Krishna sorthiya <116088459+krishna6639@users.noreply.github.com>
2025-09-05 12:15:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let form = document.querySelector('form');
 form.addEventListener('submit',function(event){
     event.preventDefault();
 });
+
+// change Event
 let user = document.querySelector("input");
-user.addEventListener('input',function(event){
-    console.log('input event');
-    console.log('final value',this.value);
+user.addEventListener('change',function(event){
+    console.log('change event');
+    console.log('final value',this.value); 
 });
